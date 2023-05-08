260d74d5c2785d7d3ce6f40f9fb5cd394f956040
javierignacioLA <javierlopezaliste@gmail.com>
2023-05-08 18:38:40 -0400

    correo electronico

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,8 +1,9 @@
-import TwitterIcon from '@mui/icons-material/Twitter';
+
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { Typography, IconButton} from '@mui/material'
 import AttachmentIcon from '@mui/icons-material/Attachment';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import MailIcon from '@mui/icons-material/Mail';
 
 const Contacto = () => {
     const handleLinkedin = () =>{
@@ -11,18 +12,16 @@ const Contacto = () => {
       const handleGitHub = () => {
         window.open('https://github.com/javierignacioLA', '_blank');
       }
-      const handleTwitter = () => {
-        window.open('https://twitter.com/javierlopezA99', '_blank');
-      }
       const handleDownload = () => {
         window.open('https://drive.google.com/file/d/13l17_o1FZSzD01PCDVKlvYnf5OfWctxz/view?usp=sharing', '_blank');
 
       };
+      const handleCorreo = () => {
+        window.open ('mailto:javierlopzaliste@gmail.com?subject=Oferta Laboral')
+
+      };
   return (
     <div><Typography variant='h6' component='div'> Twitter Card, Contacto:</Typography>
-    <IconButton aria-label="delete"  color="primary">
-        <TwitterIcon color='secondary' onClick={handleTwitter}/>
-    </IconButton>
     <IconButton aria-label="delete" color="primary">
         <LinkedInIcon color='secondary' onClick={handleLinkedin}/>
     </IconButton>
@@ -31,6 +30,9 @@ const Contacto = () => {
     </IconButton>
     <IconButton aria-label="delete" color="primary">
         <AttachmentIcon  color='secondary' onClick={handleDownload}/>
+    </IconButton>
+    <IconButton aria-label="delete" color="primary">
+        <MailIcon  color='secondary' onClick={handleCorreo}/>
     </IconButton>
     
     </div>
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,13 +1,12 @@
-
-
 import { Typography } from '@mui/material'
 
 export const Info = () => {
   return (
     <div>
-        <Typography align='justify' sx={{maxWidth:450}}>Soy Javier López Aliste, ingeniero civil informático,
+        <Typography align='justify' sx={{maxWidth:450}}>Javier López Aliste, ingeniero civil informático,
         y quise retratar un componente de twitter utilizando React junto a MaterialUI, 
-        con hosting en vercel, espero te guste, te dejo mi Curriculum en el adjunto.
+        con hosting en vercel, espero te guste, te dejo mi Curriculum en el adjunto, puedes
+        contactarme a través de correo electrónico directamente!.
       </Typography>
     </div>
   )
