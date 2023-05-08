abcc8d72efce4f305d8aa2fe54c557c639225d97
javierignacioLA <javierlopezaliste@gmail.com>
2023-05-08 18:45:19 -0400

    fix info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,8 +3,9 @@ import { Typography } from '@mui/material'
 export const Info = () => {
   return (
     <div>
-        <Typography align='justify' sx={{maxWidth:450}}>Javier López Aliste, ingeniero civil informático,
-        y quise retratar un componente de twitter utilizando React junto a MaterialUI, 
+        <Typography align='justify' sx={{maxWidth:450}}> ¡Hola! soy
+        Javier López Aliste, ingeniero civil informático,
+        quise retratar un componente de twitter utilizando React junto a MaterialUI, 
         con hosting en vercel, espero te guste, te dejo mi Curriculum en el adjunto, puedes
         contactarme a través de correo electrónico directamente!.
       </Typography>
